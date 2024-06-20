7be9a951bafad877266a83ff47d155ed1b061455
Eunchae Kim <kec9706@gmail.com>
2024-06-20 16:32:27 +0900

    authStore 코드 리펙터링 & immer 추가

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,26 +1,35 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-interface StoreState {
+interface State {
   isLogin: boolean;
   isAuthFinished: boolean; // 로그인 유무를 떠나서 인증 절차가(API 통신) 끝났는지를 나타내는 상태
+}
+
+interface Action {
   storeLogin: () => void;
   storeLogout: () => void;
   setAuthFinished: () => void;
 }
 
-export const useAuthStore = create(
-  devtools<StoreState>((set) => ({
-    isLogin: false,
-    isAuthFinished: false,
-    storeLogin: () => {
-      set({ isLogin: true });
-    },
-    storeLogout: () => {
-      set({ isLogin: false });
-    },
-    setAuthFinished: () => {
-      set({ isAuthFinished: true });
-    },
-  }))
+export const useAuthStore = create<State & Action>()(
+  immer(
+    devtools((set) => ({
+      isLogin: false,
+      isAuthFinished: false,
+
+      storeLogin: () => {
+        set({ isLogin: true });
+      },
+
+      storeLogout: () => {
+        set({ isLogin: false });
+      },
+      
+      setAuthFinished: () => {
+        set({ isAuthFinished: true });
+      },
+    }))
+  )
 );
