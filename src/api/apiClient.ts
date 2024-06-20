7dcab3065256844e1a98f86fde3ac319beee542b
Eunchae Kim <kec9706@gmail.com>
2024-06-20 18:30:56 +0900

    axios response interceptor의 예외처리 코드중 누락된 "/" 추가

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -18,7 +18,7 @@ apiClient.interceptors.response.use(
     // 토큰이 존재하지 않을경우(403), 토큰 에러(401) 처리
     if (
       [401, 403].includes(error.response.status) &&
-      !["/login", "logout", "signup"].includes(error.config.url)
+      !["/login", "/logout", "/signup"].includes(error.config.url)
     ) {
       window.location.href = "/login";
       return;
